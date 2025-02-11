d17bd87c690d570aeaa80cc339603b4db1b3e8dd
mohitpant05 <mohit1410159@gmail.com>
2025-02-11 13:02:37 +0530

    fix: runnable directory

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -2,14 +2,14 @@
 
 // import { openai } from '@ai-sdk/openai';
 import { groq } from '@ai-sdk/groq';
-import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
-
+import { experimental_wrapLanguageModel } from 'ai';
+import { wrapLanguageModel, extractReasoningMiddleware } from '../../node_modules/ai/dist/index.mjs';
 import { customMiddleware } from './custom-middleware';
 
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
     model: groq(apiIdentifier),
-    middleware: customMiddleware,
+    middleware: extractReasoningMiddleware({ tagName: 'think' }),
   });
 };
 
